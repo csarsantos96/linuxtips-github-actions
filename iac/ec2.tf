5a6cc3acb1a3a132e02c7d84336483fbc5a01f7a
Cesar Santos <csar.santos18@gmail.com>
2026-07-16 11:29:59 -0500

    fix: update resource name and remove unused variable in Terraform configuration

--- iac/ec2.tf
+++ iac/ec2.tf
@@ -1,8 +1,7 @@
-resource "aws_instance" "example" {
+resource "aws_instance" "web" {
   ami           = var.ami_id
   instance_type = var.instance_type
   
-  variavel_inexistente = var.variavel_inexistente
 
   tags = {
     Name = "HelloWorld"
--- iac/variable.tf
+++ iac/variable.tf
@@ -2,10 +2,12 @@ variable "ami_id" {
   type = string
   default = "ami-0c55b159cbfafe1f0"
 }
+
 variable "instance_type" {
   type = string
   default = "t3.micro"
-} 
+}
+
 variable "variavel_inexistente" {
   type = string
   default = "valor_inexistente"
